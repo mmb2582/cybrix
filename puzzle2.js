b01f35b8637ce6fd12a9d429ebbd8f048826fe5d
JacquelynLeung <jil4009@g.rit.edu>
2020-02-09 04:14:46 -0500

    trying things

---
+++ puzzle2.js
@@ -0,0 +1,12 @@
+function checkAnswer(){
+    a = prompt('What do you say?')
+    if(a == 'please, I don't wanna go'){
+        alert('Well Done. Correct answer')
+	window.location.replace("http://cybrix.tech/final.html");
+    }
+    else{
+        alert('Sorry, incorrect')
+	window.location.replace("http://cybrix.tech/finalnorick.html");
+    }
+
+}
